fbc16f6933ce0bc64b106c0258199051c25d00bc
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-08 16:09:39 -0700

    passing screen tests

---
+++ src/containers/ListPage.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ListPage from './ListPage';
+
+describe('List page', () => {
+  it('renders the list page', () => {
+    render(<ListPage />);
+    screen.getByText('yo yo');
+  });
+});
