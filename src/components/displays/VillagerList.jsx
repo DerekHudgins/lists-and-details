d2876c054abae83b696332b70414c4454e588364
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-08 16:57:52 -0700

    added propTypes to VillagerList

--- src/components/displays/VillagerList.jsx
+++ src/components/displays/VillagerList.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const VillagerList = () => (
-  <ul>List will go here...</ul>
-);
+const VillagerList = () => <ul aria-label="villagers">List will go here...</ul>;
 
 VillagerList.propTypes = {
-
+  villagers: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      japaneseName: PropTypes.string,
+      image: PropTypes.string,
+      phrase: PropTypes.string,
+    })
+  ),
 };
 
 export default VillagerList;
